8acfdaa59781bba30d2df582caada04f4863fa50
Easin Arafat <arafatmrx@gmail.com>
2022-01-29 11:32:25 +0600

    initial setup done

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send(`yo server is running`);
+  res.send(`Ohh yaa Blog server is running`);
 });
 
 app.listen(PORT, (error) => {
   if (!error) {
-    console.log(`server is running at ${port}`);
+    console.log(`server is running at ${PORT}`);
   }
 });
