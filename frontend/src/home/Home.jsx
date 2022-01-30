7c6f49eeceb387b6df714a6e6426434ea663e9a8
Easin Arafat <arafatmrx@gmail.com>
2022-01-30 09:34:07 +0600

    12 col  Grid with SCSS

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="row">
+        <div className="col-4">
+          <h1>1/3 of 100</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ frontend/src/home/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Its a navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
+<h1>Its a navbar</h1>;
