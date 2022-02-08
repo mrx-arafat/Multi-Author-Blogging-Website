c9eee7af8ff613360db99e1d61a0b02620e39fc5
Easin Arafat <arafatmrx@gmail.com>
2022-02-08 09:31:48 +0600

    navbar grid structure

--- frontend/src/home/Navbar.jsx
+++ frontend/src/home/Navbar.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Its a navbar</h1>
+    <div id="navbar" className="navbar">
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <div className="image-menubar">
+              <Link className="image" to="/"></Link>
+            </div>
+          </div>
+          <div className="col-8">
+            {" "}
+            <h1>Its a navbar</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Navbar;
-<h1>Its a navbar</h1>;
